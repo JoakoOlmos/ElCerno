33645d53f1cc92faa340a564996419d774dc9005
GabiiCata <gabiicata94@gmail.com>
2023-09-29 22:57:06 -0300

    relocation folder and index.html

---
+++ src/scripts/components/main.ts
@@ -0,0 +1,22 @@
+let count = 0;
+
+function updateCounter() {
+  const counterElement = document.getElementById('count');
+  if (counterElement) {
+    counterElement.textContent = count.toString();
+  }
+}
+
+function incrementCounter() {
+  count++;
+  updateCounter();
+}
+
+// Asignar el manejador de clic al botón
+const incrementButton = document.getElementById('incrementBtn');
+if (incrementButton) {
+  incrementButton.addEventListener('click', incrementCounter);
+}
+
+// Actualizar el contador inicial
+updateCounter();
