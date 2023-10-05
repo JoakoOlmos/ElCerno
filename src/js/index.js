047fc591ff42307d8ecd82f81d2bf1e7cfaf865d
GabiiCata <gabiicata94@gmail.com>
2023-10-05 19:24:19 -0300

    add excel logic with html card tamplate

---
+++ src/js/index.js
@@ -0,0 +1,77 @@
+  const excelFileUrl = "../src/docs/velcerno.xlsx";
+  // Crear una nueva instancia de FileReader
+  const reader = new FileReader();
+
+  // Configurar el evento onload para ejecutarse cuando se complete la lectura del archivo
+  reader.onload = function (e) {
+    const data = new Uint8Array(e.target.result);
+    console.log(data);
+
+    // Procesar el archivo Excel
+    const workbook = XLSX.read(data, { type: "array" });
+    const sheetName = workbook.SheetNames[0];
+    const excelData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+
+    // Llamar a las funciones para procesar y mostrar los datos
+    processExcelData(excelData);
+    displayDataInList(excelData);
+    displayDataInUi(excelData);
+  };
+
+  // Realizar la lectura del archivo desde la URL
+  fetch(excelFileUrl)
+    .then((response) => response.blob()) // Convertir la respuesta a un Blob
+    .then((blob) => reader.readAsArrayBuffer(blob))
+    .catch((error) => {
+      console.error("Error al cargar el archivo:", error);
+      alert("Error al cargar el archivo.");
+    });
+
+
+function processExcelData(data) {
+  console.log(JSON.stringify(data, null, 2));
+  // Aquí puedes realizar cualquier otra operación con los datos
+}
+
+function displayDataInList(data) {
+  const dataList = document.getElementById("dataList");
+
+  // Limpiar la lista existente, si la hay
+  dataList.innerHTML = "";
+
+  // Iterar sobre los datos y agregar elementos de lista
+  data.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = JSON.stringify(item);
+    dataList.appendChild(listItem);
+  });
+}
+
+function displayDataInUi(data) {
+  const wineListContainer = document.getElementById("wineList");
+
+  // Itera sobre cada vino en el JSON
+  // Itera sobre cada vino en el JSON
+  data.forEach((vino) => {
+    // Realizar la carga del archivo wine-card-template.html
+    fetch("./src/templates/wine-card-template.html")
+      .then((response) => response.text())
+      .then((template) => {
+        // Reemplazar las variables del template con datos reales
+        const wineCardHTML = template
+          .replaceAll("{IMAGE}", vino.Imagen)
+          .replaceAll("{NAME}", vino.Nombre)
+          .replaceAll("{VARIETAL}", vino.Varietal)
+          .replaceAll("{STOCK}", vino.Stock)
+          .replaceAll("{PRICE}", vino.Precio);
+
+        // Crear un elemento div para almacenar la tarjeta y agregarlo al contenedor
+        const wineCardContainer = document.createElement("div");
+        wineCardContainer.innerHTML = wineCardHTML;
+        wineListContainer.appendChild(wineCardContainer.firstChild);
+      })
+      .catch((error) => {
+        console.error("Error al cargar el template:", error);
+      });
+  });
+}
