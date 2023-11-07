1e4c526ca2c51f583980c9c860d3445690c61c42
JoakoOlmos <joakolmos.14@gmail.com>
2023-11-07 20:51:38 -0300

    new section 2 changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+    var mySwiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 0,
+            },
+        },
+    });
+});
